5f5985bff325b7aa7c2fce5c3addab183eee3a25
Kim hyunwoo <snltpdua@gmail.com>
2019-11-24 05:46:14 +0900

    제출...

--- client/src/actions/game.js
+++ client/src/actions/game.js
@@ -1,17 +1,29 @@
-const givePoint = ( retlname="신한 헬스클럽", aprvamt, rule="헬스클럽" ) => {
+const givePoint = () => {
     // retlname = 점포명
     // aprvamt = 이용금액
     // rule = 식별자 - 뭐 약국, 헬스 등
     // string 검사기 - var stringVal = "javascript", substring = "java"; console.log(stringVal.includes(substring));
     // let rule = "약국, 헬스클럽";
-    let healOrDamage = retlname,
-        ruler = rule;
-    console.log(healOrDamage.includes(ruler));
+    let retlname="신한 헬스클럽";
+    let aprvamt="100000";
+    let rule="헬스클럽";
+    let originalPoint = 10000;
+    
+    let judge = retlname.includes(rule);
 
     // 조건에 맞는 소비일 경우 결제금액의 0.02%만큼 포인트를 더한 후 originalPoint에 업데이트
-    if(healOrDamage.includes(ruler) === true) {
-        let originalPoint = originalPoint + aprvamt*0.02
+    const punish = () => {
+        let pointAll = []
+    if(judge === true) {
+       let pointF = originalPoint + (aprvamt*0.02)
+       pointAll.push(originalPoint, pointF) 
     } else {
-        let originalPoint = originalPoint - aprvamt*0.02
-    }
+       let pointF = originalPoint - (aprvamt*0.02)
+       pointAll.push(originalPoint, pointF)
+    } 
+    return pointAll;
+    
+}
+    console.log(originalPoint);
+    punish();
 }
